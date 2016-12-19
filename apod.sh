1083b3e370c95b9aecfc0566a68a1a961ead32d1
hidroxido <hidroxido6@gmail.com>
2016-12-18 22:35:57 -0300

    Update apod.sh

--- apod.sh
+++ apod.sh
@@ -1,4 +1,21 @@
 #! /bin/bash
+#AWOD Copyright (C) 2016 Camilo Ortiz
+#apod.nasa.gov Copyright (C) NASA and  Robert Nemiroff (MTU) & Jerry Bonnell (UMCP)
+#Pictures Copyright (C) belong to their respective owners
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>
+#===============================================================
 
 #CAMBIAR PATH PARA UBICACION DE IMAGEN DE FONDO
 ubicacion=/home/usuario/wallpaper
