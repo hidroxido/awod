22a6c8a95ed586321a82f09a47159d04c19adca9
hidroxido <hidroxido6@gmail.com>
2016-12-18 21:40:43 -0300

    Update apod.sh

--- apod.sh
+++ apod.sh
@@ -1,16 +1,31 @@
 #! /bin/bash
 
-wget -q http://apod.nasa.gov -P /home/saruman/wallpaper
-nasa=$(grep "<IMG" /home/saruman/wallpaper/index.html | cut -d \" -f 2 )
+#CAMBIAR PATH PARA UBICACION DE IMAGEN DE FONDO
+ubicacion=/home/usuario/wallpaper
+
+wget -q http://apod.nasa.gov -P $ubicacion
+nasa=$(grep "<IMG" $ubicacion/index.html | cut -d \" -f 2 )
 apod="http://apod.nasa.gov/apod/"$nasa
 echo $apod
 image=$(echo $nasa | cut -d "/" -f 3)
 fecha=$(date)
-echo "" >> apod.log
-echo "========= $fecha ==========" >> apod.log
-wget -nv $apod -P /home/saruman/wallpaper >> apod.log 2>&1
-rm /home/saruman/wallpaper/index.html
-cp /home/saruman/wallpaper/$image /home/saruman/wallpaper/apod/
-mv /home/saruman/wallpaper/$image /home/saruman/wallpaper/bg.jpg
-DISPLAY=:0.0 fbsetbg -c /home/saruman/wallpaper/bg.jpg
+echo "" >> $ubicacion/apod.log
+echo "========= $fecha ==========" >> $ubicacion/apod.log
+wget -nv $apod -P $ubicacion >> $ubicacion/apod.log 2>&1
+rm $ubicacion/index.html
+
+#COMPROBAR SI EXISTE DIRECTORIO apod/ PARA GUARDAR IMAGNES ORGINALES
+if [ -d $ubicacion/apod/ ];
+  then
+    cp $ubicacion/$image $ubicacion/apod/
+  else
+    mkdir $ubicacion/apod/
+    cp $ubicacion/$image $ubicacion/apod/
+fi
+
+#LA IMAGEN QUE SE OCUPARA DE FONDO SE LLAMARA bg.jpg
+mv $ubicacion/$image $ubicacion/bg.jpg
+
+#IMPORTANTE !!!! CAMBIAR SEGUN ESCITORIO O DESKTOP MANAGER QUE SE UTILICE !!!
+DISPLAY=:0.0 fbsetbg -c /home/saruman/wallpaper/bg.jpg #METODO PARA CAMBIAR FONDO EN FLUXBOX
 
